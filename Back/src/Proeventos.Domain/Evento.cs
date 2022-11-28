374fb1ecbb9e472c4122cf7e66245a4f3b5b0328
PauloJsReact <paulo_henrique_araujo@outlook.com>
2022-11-28 11:02:40 -0300

    Database Update

---
+++ Back/src/Proeventos.Domain/Evento.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Proeventos.Domain;
+
+namespace ProEventos.Domain
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string ImagemUrl { get; set; }
+        public IEnumerable<Lote> Lotes { get; set; }
+        public IEnumerable<RedeSocial> RedesSocials { get; set; }
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+
+    }
+}
---
+++ Back/src/Proeventos.Domain/PalestranteEvento.cs
@@ -0,0 +1,12 @@
+using ProEventos.Domain;
+
+namespace Proeventos.Domain
+{
+    public class PalestranteEvento
+    {
+        public int PalestranteId { get; set; }
+        public Palestrante Palestrante { get; set; }
+        public int EventoId { get; set; }
+        public Evento Evento { get; set; }
+    }
+}
